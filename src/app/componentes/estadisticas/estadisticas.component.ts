3e87f234c36d0a2bf9b68457b6bbc2076c600f5f
Fredy Alejandro Russi Fajardo <frussi@asesoftware.com>
2023-03-15 02:42:06 -0500

    Ajustes finales.

--- src/app/componentes/estadisticas/estadisticas.component.ts
+++ src/app/componentes/estadisticas/estadisticas.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-estadisticas',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EstadisticasComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
   }
+
+  /*regresar(){
+    this.router.navigate
+
+  }*/
 
 }
